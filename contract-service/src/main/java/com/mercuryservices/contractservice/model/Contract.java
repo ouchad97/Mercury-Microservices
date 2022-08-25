8ef84bc237e383fcc5c7472c96b68131b40feb0c
ouchad97 <ouchad97@gmail.com>
2022-08-25 14:51:21 +0100

    Until the last part of the 20th century, Mercury was one of the least-understood planets, and even now the shortage of information about it leaves many basic questions unsettled. Indeed, the length of its day was not determined until the 1960s

---
+++ contract-service/src/main/java/com/mercuryservices/contractservice/model/Contract.java
@@ -0,0 +1,28 @@
+package com.mercuryservices.contractservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "t_contract")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idContract;
+
+    /**/
+    private String contractId;
+    private Date datecreationContract;
+    private String libelleContract;
+
+
+}
