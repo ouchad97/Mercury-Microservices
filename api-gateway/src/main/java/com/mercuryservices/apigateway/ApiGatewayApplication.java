f426983016c9697a46c1c9fa47040eba76bc89ab
ouchad97 <ouchad97@gmail.com>
2022-08-26 13:13:24 +0100

    add Gateway dependencies...

--- api-gateway/src/main/java/com/mercuryservices/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mercuryservices/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,9 @@
 package com.mercuryservices.apigateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
