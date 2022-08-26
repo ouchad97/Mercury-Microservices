fa992aabd183e08638f31bd09be238288ba7e80b
ouchad97 <ouchad97@gmail.com>
2022-08-26 13:12:10 +0100

    Create class ApiGateway and Enabling eurekaClient

---
+++ api-gateway/src/main/java/com/mercuryservices/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.mercuryservices.apigateway;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+
+    }
+}
