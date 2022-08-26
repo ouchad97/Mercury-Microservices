50e95d30834cf3c698ca803540baf484c43a11ba
ouchad97 <ouchad97@gmail.com>
2022-08-26 15:08:29 +0100

    springfox-swagger2

--- fiche-service/src/main/java/com/mercuryservices/ficheservice/FicheServiceApplication.java
+++ fiche-service/src/main/java/com/mercuryservices/ficheservice/FicheServiceApplication.java
@@ -2,8 +2,10 @@ package com.mercuryservices.ficheservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FicheServiceApplication {
 
 	public static void main(String[] args) {
