65711f5687c6e79942e4b55fe31adb80d8925161
ouchad97 <ouchad97@gmail.com>
2022-08-26 15:18:48 +0100

    Link Swagger to all project

--- contract-service/src/main/java/com/mercuryservices/contractservice/ContractServiceApplication.java
+++ contract-service/src/main/java/com/mercuryservices/contractservice/ContractServiceApplication.java
@@ -2,12 +2,12 @@ package com.mercuryservices.contractservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaServer
+@EnableSwagger2
 public class ContractServiceApplication {
 
     public static void main(String[] args) {
--- devis-service/src/main/java/com/mercuryservices/devisservice/DevisServiceApplication.java
+++ devis-service/src/main/java/com/mercuryservices/devisservice/DevisServiceApplication.java
@@ -2,12 +2,12 @@ package com.mercuryservices.devisservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaServer
+@EnableSwagger2
 public class DevisServiceApplication {
 
     public static void main(String[] args) {
